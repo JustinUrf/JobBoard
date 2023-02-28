295eade03d31c7fa6fc25e136d8cd7940d8b8f1a
Justin Lee <lee.justin001126@gmail.com>
2023-02-28 15:02:34 -0800

    add more content in Jobs\Index.cshtml
    Co-authored-by: Sarah Andyshak <sarah.andyshak@gmail.com>

--- JobBoard/Controllers/JobsControllers.cs
+++ JobBoard/Controllers/JobsControllers.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 
 namespace JobBoard.Controllers
 {
-//     public class JobsController : Controller
-//     {
-//       [HttpGet("/jobs")]
-//       public ActionResult Index()
-//       {
-//         List<Job> allJobs = JobBoard.GetAll();
-//         return View(allJobs);
-//       }
-//     }
+    public class JobsController : Controller
+    {
+      [HttpGet("/jobs")]
+      public ActionResult Index()
+      {
+        List<Job> allJobs = JobBoard.GetAll();
+        return View(allJobs);
+      }
+    }
 }
